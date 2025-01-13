f641bae1746c7ecbe4732090660e1f9914558f49
Komal514 <233520@students.au.edu.pk>
2025-01-13 04:27:24 -0800

    Add files via upload

---
+++ Assignment05/Activity_01/Components/Models/CitiesRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Activity_01.Components.Models
+{
+    public class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+           "Montreal",
+           "Ottama",
+           "Calgary",
+           "Halifex"
+
+
+        };
+        public static List<string> GetCities() => cities;
+
+    }
+}
---
+++ Assignment05/Activity_01/Components/Models/ServersRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+
+namespace Activity_01.Components.Models
+{
+    public class ServersRepository
+    {
+        private static List<Server> servers = new List<Server>
+        {
+            new Server { Name = "Server1", City = "Toronto", IsOnline = true },
+            new Server { Name = "Server2", City = "Toronto", IsOnline = true },
+            new Server { Name = "Server3", City = "Toronto", IsOnline = true },
+            new Server { Name = "Server4", City = "Toronto", IsOnline = false },
+            new Server { Name = "Server1", City = "Montreal", IsOnline = true },
+        };
+
+        public static List<Server> GetServersByCity(string city)
+        {
+            return servers.Where(s => s.City == city).ToList();
+        }
+    }
+}
