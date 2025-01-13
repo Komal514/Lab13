60e9a39e9cb2a4de1f2a85476819335457664548
Komal514 <233520@students.au.edu.pk>
2025-01-13 04:34:53 -0800

    Add files via upload

---
+++ Activity_01/Activity_01/Components/Models/Server.cs
@@ -0,0 +1,13 @@
+﻿namespace Activity_01.Components.Models
+{
+    public class Server
+    {
+        public string Name { get; set; }
+
+        // The city where the server is located
+        public string City { get; set; }
+
+        // Indicates whether the server is online or offline
+        public bool IsOnline { get; set; }
+    }
+}
